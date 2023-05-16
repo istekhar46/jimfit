95df3da09fbed807533256ee97536ef8f5813053
istekhar khan <istekharkhan46@gmail.com>
2023-05-17 00:14:27 +0530

    added news component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import News from './components/News';
 
 function App() {
   return (
+    <>
    <Navbar/>
+   <News/>
+   </>
   )
 }
 
---
+++ src/components/Spinning.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Search from '../gifs/Fading circles.gif'
+
+const Spinnig =()=> {
+    return (
+        <div className='text-center'>
+      <img style={{height:'15px',width:'auto'}} src={Search} alt="loading.." />
+      </div>
+    )}
+
+export default Spinnig
