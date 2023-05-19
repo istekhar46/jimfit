7221f710c4f75f20fceecc15b3159f0ac8da41eb
Aman Sirohi <amansirohi077@gmail.com>
2023-05-19 14:04:29 +0530

    added readme fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import Logo from '../assets/images/Logo-1.jpg';
 
+
 const Footer = () => (
   <>
     <Box mt="80px" className='boxShadow: 0pc 0.5px 0.5px'>
       <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="0" pt="24px">
         <img src={Logo} alt="logo" style={{ width: '200px', height: '200px', borderRadius: '50%' }} />
       </Stack>
-      <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">
+      {/* <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="10px">
 
-      </Typography>
+      </Typography> */}
 
-
+<p className='text-center'>&copy; Made by <a href="https://github.com/amansirohiii" target="_blank" rel="noopener noreferrer">Aman</a> and <a href="https://github.com/istekhar46" target="_blank" rel="noopener noreferrer">Istekhar</a></p>
     </Box>
+
 
   </>
 );
